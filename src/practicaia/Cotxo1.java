1f68b4aee2e4e4eec32006517388e9969ca320a2
Aysha <ayshag611@gmail.com>
2020-10-25 12:55:18 +0100

    esquiva taques d'oli y cambia angulo de visores
    cambia angulo visores según recta o curva
    a veces recoge los recursos

--- src/practicaia/Cotxo1.java
+++ src/practicaia/Cotxo1.java
@@ -15,19 +15,13 @@ public class Cotxo1 extends Agent {
     static final int COTXE = 1;
     static final int PARED = 0;
 
-    int numDreta = 0;
-    int numEsquerra = 0;
-
     int VELOCITATTOPE = 5;
     int VELOCITATFRE = 4;
 
     Estat estat;
     int espera = 0;
     double desquerra, ddreta, dcentral;
-
-    boolean aDreta; //(true si la distancia mes curta es la taca de la dreta)
     double distancia;
-    double distanciaMin;
 
     public Cotxo1(Agents pare) {
         super(pare, "Cotxo", "imatges/CotxoV.png");
@@ -35,95 +29,48 @@ public class Cotxo1 extends Agent {
 
     @Override
     public void inicia() {
-        setAngleVisors(40);
+        setAngleVisors(10);
         setDistanciaVisors(350);
         setVelocitatAngular(9);
     }
 
     @Override
     public void avaluaComportament() {
+       
         estat = estatCombat();  // Recuperam la informació actualitzada de l'entorn
 
         if (espera > 0) {  // no facis res, continua amb el que estaves fent
             espera--;
             return;
         }
-        setVelocitatAngular(9);
         ddreta = estat.distanciaVisors[DRETA];
         desquerra = estat.distanciaVisors[ESQUERRA];
         dcentral = estat.distanciaVisors[CENTRAL];
-        numDreta = 0;
-        numEsquerra = 0;
+        
 
         if (estat.revolucions > 2500 && estat.marxa < 5 && dcentral > 200) {
             endavant(estat.marxa + 1);
+            setAngleVisors(10);
         } else {
             endavant(estat.marxa);
             espera = 1;  // esto que es?
-        }
+        }  
 
-        for (int i = 0; i < estat.numObjectes; i++) {
-            if (estat.objectes[i].tipus == Agent.TACAOLI) {
-                distancia = estat.posicio.distancia(estat.objectes[i].posicio);
-                if (distancia < 100) {
-                    //System.out.println("Distancia taca oli " + distancia);
-                    if (distancia < distanciaMin) {
-                        distanciaMin = distancia;
-                    }
-                    if (estat.objectes[i].sector >= 3) {
-                        numDreta++;
-                        if (distancia > distanciaMin) {
-                            aDreta = true;
-                        }
-                    } else if (estat.objectes[i].sector <= 2 && estat.objectes[i].sector > 0) {
-                        numEsquerra++;
-                        if (distancia == distanciaMin) {
-                            aDreta = false;
-                        }
-                    }
-                }
-            }
-        }
-        if (numDreta > 0 || numEsquerra > 0) {
-            if (aDreta) {
-                esquerra();
-            } else {
-                dreta();
-            }
-        }
-        if (dcentral > 160 && (desquerra > 20) && (ddreta > 20)) {
+        if (dcentral > 200 && (desquerra > 20) && (ddreta > 20)) {
             noGiris();
-
         } else {
             if (estat.marxa >= VELOCITATFRE) {
                 endavant(estat.marxa - 1);
                 espera = 1;
             }
-            //if (ddreta != desquerra) {
-            // setVelocitatAngular(8);
-
+            setAngleVisors(40);
             if (ddreta > desquerra) {
-                if (numDreta > 0 && aDreta) {
-                    esquerra();
-                } else {
-                    dreta();
-                }
+                dreta();
             } else {
-                if (numEsquerra > 0 && !aDreta) {
-                    dreta();
-                } else {
-                    esquerra();
-                }
+                esquerra();
             }
-            //}
         }
 
-        if (dcentral > 170) {
-            //endavant(VELOCITATTOPE);
-            setVelocitatAngular(6);
-        } else {
-            setVelocitatAngular(9);
-        }
         // no ir en contra dirección :)
         if (estat.contraDireccio) {
             espera = 30;
@@ -138,11 +85,11 @@ public class Cotxo1 extends Agent {
         // cuando choca con coche o pared
         if (estat.enCollisio) {
             //choca por detras
-            if ((estat.distanciaVisors[CENTRAL] > 15) && (estat.distanciaVisors[ESQUERRA] > 15) && (estat.distanciaVisors[DRETA] > 15)) {
+            if ((estat.distanciaVisors[CENTRAL] > 15) && 
+               (estat.distanciaVisors[ESQUERRA] > 15) &&
+               (estat.distanciaVisors[DRETA] > 15)) {
                 endavant(1);
-            } else { //cocha por delante
-
-                //if (estat.objecteVisor[CENTRAL] == PARED || estat.objecteVisor[ESQUERRA] == PARED || estat.objecteVisor[DRETA] == PARED){
+            } else { //choca por delante
                 espera = 30;
                 noGiris();
                 enrere(2);
@@ -153,6 +100,27 @@ public class Cotxo1 extends Agent {
         if (estat.objecteVisor[CENTRAL] == COTXE) {
             dispara();
         }
-    }
 
+        for (int i = 0; i < estat.numObjectes; i++) {
+            if (estat.objectes[i].tipus == Agent.TACAOLI) {
+                distancia = estat.posicio.distancia(estat.objectes[i].posicio);
+                if (distancia < 100) {
+                    if (estat.objectes[i].sector == 2){
+                        esquerra();
+                    }else if (estat.objectes[i].sector == 3){
+                        dreta();
+                    }
+                }
+            }else if (estat.objectes[i].tipus == Agent.RECURSOS  && estat.fuel < 2000) {
+                distancia = estat.posicio.distancia(estat.objectes[i].posicio);
+                if (distancia < 100) {
+                    if (estat.objectes[i].sector == 1 ){
+                        dreta();
+                    }else if (estat.objectes[i].sector == 4){
+                        esquerra();
+                    }
+                }
+            }
+        }
+    }
 }
